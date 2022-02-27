e484f3575585c740d98739fa2df31d36516668c7
LiangChen001 <690266235@qq.com>
2022-02-27 16:19:05 +0800

    完善

--- frap-web/src/pages/NotFound/index.jsx
+++ frap-web/src/pages/NotFound/index.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import {useNavigate} from "react-router-dom";
+import './index.css'
 function Index(props) {
+	const navgiate = useNavigate()
+	let [time,setTime] = useState(5)
+	useEffect(()=>{
+		const timer = setInterval(()=>{
+			setTime(time-=1)
+			if(time === 0){
+				clearInterval(timer)
+				navgiate('/')
+			}
+		},1000)
+	})
 	return (
-		<div>page not found</div>
+		<div className="container">
+			<div className="content">
+				<div className="title">404</div>
+				<div>{time}秒后返回首页</div>
+			</div>
+		</div>
 	);
 }
 
